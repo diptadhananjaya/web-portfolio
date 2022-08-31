cb9d7ad3f3a05c121ddca4df30dbc64679ba2682
Dipta Dhananjaya <madedipta88@gmail.com>
2022-08-31 13:39:34 +0800

    home section

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import "./index.css";
+import hero from "../../assets/Programming-pana.png";
 
-const index = () => {
+const Index = () => {
   return (
-    <div>
+    <div className="wrapHome">
       <Navbar />
+      <div className="homeContainer">
+        <div className="introduction">
+          <h1>Hi There!👋 </h1>
+          <p>I'm Dipta Dhananjaya</p>
+          <button className="btnAbout">About Me</button>
+        </div>
+        <img src={hero} alt="" className="imgHero" />
+      </div>
     </div>
   );
 };
 
-export default index;
+export default Index;
